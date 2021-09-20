fbca5f1f7f52a9bfbaf84d2e70d3c7572a591c72
Albina <reitsukiko@yandex.ru>
2021-09-20 03:27:17 +0300

    lesson1

--- 1ls2.rb
+++ 1ls2.rb
@@ -2,7 +2,7 @@ puts "Set the base(a) of the triangle: "
 a = gets.chomp.to_i
 
 puts "Set the base(h) of the triangle: "
-b = gets.chomp.to_i
+h = gets.chomp.to_i
 
 s = (0.5 * a * h)
   puts "The area(S) pf the created triangle is equal to #{s}."
--- 1ls3.rb
+++ 1ls3.rb
@@ -12,7 +12,7 @@ a, b, c = array.sort
  
 if (a == b) && (b == c) && (c == a)
   puts "The triangle is isosceles and equilateral, but not rectangular."
-elsif c**2 == a**2 + a**2
+elsif c**2 == a**2 + b**2
   puts "Rectangular triangle."
 elsif (a == b) || (a == c) || (b == c)
   puts "Isosceles triangle."
